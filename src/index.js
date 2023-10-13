5558256f9b022ea9a135cbf5f902d415179f32fd
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-10-13 14:06:31 -0600

    added image and css loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 console.log("Things seem to be in order here");
+
+/* Footer stuff
+Monkey icon: <a href="https://www.flaticon.com/free-icons/monkey" title="monkey icons">Monkey icons created by Freepik - Flaticon</a>
+parrot: <a href="https://www.flaticon.com/free-icons/parrot" title="parrot icons">Parrot icons created by Mihimihi - Flaticon</a>
+*/
