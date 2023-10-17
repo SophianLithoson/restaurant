3aa815d4b8fa4c3c2736bd45d4a29590d801c3c5
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-10-17 12:40:17 -0600

    completed menu tab, added margin between header and main

--- src/menu.js
+++ src/menu.js
@@ -1,14 +1,34 @@
-import harvestBowlPic from "./harvest_bowl.jpg";
-import naanPic from "./naan.jpg";
-import tanBinhPic from "./tan_binh.jpg"
+import harvestBowlPic from "./harvest_bowl_s.jpg";
+import naanPic from "./Naan_s.jpg";
+import tanBinhPic from "./tan_binh_s.jpg";
 
 function menuContent() {
     console.log("function menuContent() triggered");
 
     const contentContainer = document.getElementById("content-container");
-    const newContentCard = document.createElement("div");
-
     contentContainer.innerHTML = "";
+
+    const firstMenuItem = document.createElement("div");
+    firstMenuItem.classList.add("content-card");
+    firstMenuItem.appendChild(Object.assign(new Image(120, 120), {src: naanPic, style: "float: left; margin-right: 5px; object-fit: cover;"}));
+    firstMenuItem.appendChild(Object.assign(document.createElement("h1"), {innerHTML: "Naan - 5"}));
+    firstMenuItem.appendChild(Object.assign(document.createElement("p"), {innerHTML: "Our fresh naan is to die for! Brushed with garlic butter and served with your choice of sauces or chutney"}));
+    contentContainer.appendChild(firstMenuItem);
+
+    const secondMenuItem = document.createElement("div");
+    secondMenuItem.classList.add("content-card");
+    secondMenuItem.appendChild(Object.assign(new Image(120, 120), {src: harvestBowlPic, style: "float: left; margin-right: 5px; object-fit: cover;"}));
+    secondMenuItem.appendChild(Object.assign(document.createElement("h1"), {innerHTML: "Harvest Bowl - 10"}));
+    secondMenuItem.appendChild(Object.assign(document.createElement("p"), {innerHTML: "Our House specialty. Strips of fried sweet potatos covered in local produce and served with chutney and raita."}));
+    contentContainer.appendChild(secondMenuItem);
+
+
+    const thirdMenuItem = document.createElement("div");
+    thirdMenuItem.classList.add("content-card");
+    thirdMenuItem.appendChild(Object.assign(new Image(120, 120), {src: tanBinhPic, style: "float: left; margin-right: 5px; object-fit: cover;"}));
+    thirdMenuItem.appendChild(Object.assign(document.createElement("h1"), {innerHTML: "Tan Binh - 12"}));
+    thirdMenuItem.appendChild(Object.assign(document.createElement("p"), {innerHTML: "A delicious stew inpired by cuisine from the Tan Binh region of Vietnam. Served with prawns, local chilies, and fish balls in a rich seafood broth"}));
+    contentContainer.appendChild(thirdMenuItem);
 }
 
 export default menuContent;
