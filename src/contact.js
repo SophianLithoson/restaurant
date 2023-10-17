c006c8a6ed21551b89dd206a164e5f527d8552e9
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-10-16 20:59:25 -0600

    started working on menu.js

--- src/contact.js
+++ src/contact.js
@@ -9,7 +9,7 @@ function contactContent() {
     contentContainer.innerHTML = "";
     newContentCard.classList.add("content-card");
     newContentCard.style = "min-width: 380px;"
-    newContentCard.appendChild(Object.assign(new Image(150, 150), {src: monkeyIcon, style: "float: left; padding-right: 5px;"}));
+    newContentCard.appendChild(Object.assign(new Image(110, 110), {src: monkeyIcon, style: "float: left; padding-right: 5px;"}));
     newContentCard.appendChild(Object.assign(document.createElement("h1"), {innerHTML: "Contact Us"}));
     newContentCard.appendChild(Object.assign(document.createElement("p"), {innerHTML: "Telephone: (888) 555-5555"}));
     newContentCard.appendChild(Object.assign(document.createElement("p"), {innerHTML: "Email: very@real.com"}));
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,6 @@
-import weedsIcon from "./weeds.png";
+import harvestBowlPic from "./harvest_bowl.jpg";
+import naanPic from "./naan.jpg";
+import tanBinhPic from "./tan_binh.jpg"
 
 function menuContent() {
     console.log("function menuContent() triggered");
