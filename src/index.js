3930a8b916363999a1e29502e2f00b4302101981
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-10-16 18:58:07 -0600

    moved home page content generator to it's own module and imported into index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,7 @@
 import "./style.css";
+import homeContent from "./home.js";
 
-pageContent();
-
-function pageContent() {
-    const contentContainer = document.getElementById("content-container");
-    const newContentCard = document.createElement("div");
-    const newCCHeading = document.createElement("h1");
-    const newCCText = document.createElement("p");
-
-    newContentCard.classList.add("content-card");
-    newCCHeading.textContent = "Welcome to the Jungle!";
-    newCCText.textContent = "Here at Assam Jungle Cafe, we bring you the best cuisine from all across India. Inspired by the the cuisine of my home villiage located near the Dehing Patkai rainforest, Assam Jungle Cafe is your new source for authentic Indian food!";
-    newContentCard.appendChild(newCCHeading);
-    newContentCard.appendChild(newCCText);
-    contentContainer.appendChild(newContentCard);
-
-    // newContentCard.removeChild(newCCText);
-    // newContentCard.removeChild(newCCText);
-    const secondContentCard = document.createElement("div");
-    secondContentCard.classList.add("content-card");
-    secondContentCard.appendChild(Object.assign(document.createElement("h1"), {innerHTML:"Operating Hours"}));
-    secondContentCard.appendChild(Object.assign(document.createElement("p"), {innerHTML:"Sunday: 8am - 2pm"}));
-    secondContentCard.appendChild(Object.assign(document.createElement("p"), {innerHTML:"Monday: 10am - 9pm"}));
-    secondContentCard.appendChild(Object.assign(document.createElement("p"), {innerHTML:"Tuesday: 10am - 9pm"}));
-    secondContentCard.appendChild(Object.assign(document.createElement("p"), {innerHTML:"Wednesday: 10am - 9pm"}));
-    secondContentCard.appendChild(Object.assign(document.createElement("p"), {innerHTML:"Thursday: 10am - 9pm"}));
-    secondContentCard.appendChild(Object.assign(document.createElement("p"), {innerHTML:"Friday: 10am - 11pm"}));
-    secondContentCard.appendChild(Object.assign(document.createElement("p"), {innerHTML:"Saturday: 8am - 11pm"}));
-
-    contentContainer.appendChild(secondContentCard);
-}
+homeContent();
 
 console.log("Things seem to be in order here");
 
